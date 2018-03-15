e5bedeb963c8c349225990028e51ead8a3ebc28f
Jennifer Ponder <jponder2000@yahoo.com>
2018-03-14 21:59:03 -0400

    wipupdated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,28 +4,64 @@
 //Countdown timer needs to be added to the page
 //The game ends when the timer runs out 
 //====================================================================
-
+//VARIABLES
 var correctAnswer = []
 var incorrectAnswer = []
 var playerChoice = '';
-var questions = 0
+var timer = 25;
+var score = 0;
+var question = 0;
+var questionArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6']
+// var questions = 0
 
 //==================================================================
 //Array within an object...this may be helpful?
-var question1 = 'What was the name of the girl on small wonder?';
-var questionOptions = ['Vicki', 'Danielle', 'Samantha', 'Nicole'];
-var questionAnswer = 'Vicki';
-var playerChoice = ''; 
+// var question1 = 'What was the name of the girl on small wonder?';
+// var questionOptions = ['Vicki', 'Danielle', 'Samantha', 'Nicole'];
+// var questionAnswer = 'Vicki';
+// var playerChoice = ''; 
 
-var questions = [
- question: 'What was the name of the girl on small wonder?',
- options: ['Vicki', 'Danielle', 'Samantha', 'Nicole'],
- answer: 'Vicki',
+var questions = 
+[
+  {
+	 question1: 'What was the name of the girl on small wonder?',
+	 options: ['Vicki', 'Danielle', 'Samantha', 'Nicole'],
+	 answer: 'Vicki'
+	},
+	{
+	  question2: 'On Threes Company, what city did the three rommates live in?',
+	  options: ['Los Angeles', 'Chicago', 'New Jersey', 'San Diego'],
+	  answer: 'San Diego'
+	},
+	{
+	  question3: 'What was the name of Punky Brusters dog?',
+	  options: ['Lena', 'Brandon', 'Spot', 'Cookie'],
+	  answer: 'Brandon'
+	},
+	{
+	  question4: 'What was the full name of Magnum P.I.?',
+	  options: ['Oscar Strong', 'Michael Knight', 'Thomas Magnum', 'Carl Pierce'],
+	  answer: 'Thomas Magnum'
+	},
+	{
+	  question5: 'What was the name of the principal on Saved By the Bell?',
+	  options: ['Richard Belding', 'Tommy Lawson', 'Richard Smalls', 'Austin Love'],
+	  answer: 'Richard Belding'	
+	},
+	{
+	  question6: 'What was the occupation of Claire Huxstable on The Cosby Show?',
+	  options: ['Dentist', 'Doctor', 'Lawyer', 'Scientist'],
+	  answer: 'Lawyer'
+	}
 ]
-
 
 //==================================================================
 //Check answers to see if corect
+
+function gameStart() {
+
+}
+
 
 function checkAnswers(){
 	if (question1 == "Vicki") {
